fb78fb59cea025fe31d3fb7b9a12714b22ad2603
dushyant615 <dushyant.bhavsar15@gmail.com>
2021-12-29 14:57:53 +0530

    added feature,core feature and workflow in features section

--- src/sections/key-feature.js
+++ src/sections/key-feature.js
@@ -48,7 +48,7 @@ export default function KeyFeature() {
    <section sx={{variant: 'section.keyFeature'}} id="feature">
      <Container>
        <SectionHeader
-          slogan="Whats the function"
+          slogan="Key Features"
           title="Meet the feature of our product"       
        />
        <Grid sx={styles.grid}>
